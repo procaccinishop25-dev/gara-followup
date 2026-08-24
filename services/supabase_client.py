2cd9cb9c4333e6268d09915753040abd6b221e7e
procaccinishop25-dev <procaccinishop25@gmail.com>
2026-08-24 22:03:38 +0200

    Create supabase_client.py

---
+++ services/supabase_client.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from supabase import create_client, Client
+
+
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+
+def get_supabase_client() -> Client:
+    return create_client(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    )
