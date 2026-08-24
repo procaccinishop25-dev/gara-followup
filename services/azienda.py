c90d88f92bfd0e2ca1866a184b6c6166c5e4e9bd
procaccinishop25-dev <procaccinishop25@gmail.com>
2026-08-24 22:20:08 +0200

    Update azienda.py

--- services/azienda.py
+++ services/azienda.py
@@ -8,3 +8,21 @@ def get_mia_azienda_id(supabase: Client):
     ).execute()
 
     return response.data
+
+
+def get_mia_azienda(supabase: Client):
+
+    azienda_id = get_mia_azienda_id(
+        supabase
+    )
+
+    response = (
+        supabase
+        .table("azienda")
+        .select("id, nome")
+        .eq("id", azienda_id)
+        .single()
+        .execute()
+    )
+
+    return response.data
